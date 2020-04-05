46f79f3d74998721def92a0bf79638e8c81d14af
mattrafalko@gmail.com <mattrafalk@gmail.com>
2020-04-04 23:48:35 -0500

    Updates

---
+++ precache-manifest.c96dd9b4fa926e635febe16913b18db1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "604c561a016fb41059c77812567abd57",
+    "url": "/coronaInfoSite/index.html"
+  },
+  {
+    "revision": "4b66fd5c970ab0024880",
+    "url": "/coronaInfoSite/static/css/main.fa1a7c8f.chunk.css"
+  },
+  {
+    "revision": "ef06be93f41f393a554e",
+    "url": "/coronaInfoSite/static/js/2.68120f88.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/coronaInfoSite/static/js/2.68120f88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b66fd5c970ab0024880",
+    "url": "/coronaInfoSite/static/js/main.2148d4bb.chunk.js"
+  },
+  {
+    "revision": "9b2dc4e8223aec9eca16",
+    "url": "/coronaInfoSite/static/js/runtime-main.6bbc0cac.js"
+  },
+  {
+    "revision": "c3382ee36ed9533b9fb901692a02ba69",
+    "url": "/coronaInfoSite/static/media/appleCovid.c3382ee3.jpeg"
+  },
+  {
+    "revision": "247d37f0ff0bcac1d8c42b4002218d36",
+    "url": "/coronaInfoSite/static/media/errorman.247d37f0.jpg"
+  },
+  {
+    "revision": "0137d1d1c24accce6a536766d84df08b",
+    "url": "/coronaInfoSite/static/media/statue.0137d1d1.jpg"
+  },
+  {
+    "revision": "849165867c8d848dad1f23e17cde81b0",
+    "url": "/coronaInfoSite/static/media/ywca.84916586.png"
+  }
+]);
